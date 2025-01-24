331fa6b000307404e7be008691997c3f0830a2d7
Pho3nyxX <60277772+Pho3nyxX@users.noreply.github.com>
2025-01-24 18:33:09 -0500

    tests updates

--- cypress/e2e/tests/linked-list-page-load.cy.js
+++ cypress/e2e/tests/linked-list-page-load.cy.js
@@ -6,17 +6,17 @@ describe("Linked list page", () => {
     
     it("should load after 'linked list' menu click", () => {
         cy.get(".menu-title").should("be.visible");
-        cy.get("#data-structures").click();
-        cy.get("#sub-menu-1").should("be.visible");
-        cy.get("#sub-menu-1 li:nth-child(2)").click();
+		cy.get(".menu-container>.menu>li:first-child >a").click();
+        cy.get(".menu-container>.menu>li:first-child ul").should("be.visible");
+        cy.get(".menu-container>.menu>li:first-child ul li:nth-child(2)").click();
         // verify redirect
     });
 
-    it.only("title should display - linked list", () => {
+    it("title should display - linked list", () => {
         cy.get(".menu-title").should("be.visible");
-        cy.get("#data-structures").click();
-        cy.get("#sub-menu-1").should("be.visible");
-        cy.get("#sub-menu-1 li:nth-child(2)").click();
+		cy.get(".menu-container>.menu>li:first-child >a").click();
+        cy.get(".menu-container>.menu>li:first-child ul").should("be.visible");
+        cy.get(".menu-container>.menu>li:first-child ul li:nth-child(2)").click();
         cy.title().should('include', 'linked list');
     });
 });
