5d67e5e0cc15c4059178517217f8504f533fb3f1
Pho3nyxX <60277772+Pho3nyxX@users.noreply.github.com>
2025-01-24 18:24:38 -0500

    tests updates

--- cypress/e2e/tests/menu.cy.js
+++ cypress/e2e/tests/menu.cy.js
@@ -13,60 +13,60 @@ describe("Menu", () => {
         cy.viewport(512, 768);
 		cy.get("nav .nav-toggle-container").should("be.visible");
 		cy.get("nav .nav-toggle-container").click();
-		cy.get("#data-structures").click();
-		cy.get("#sub-menu-1").should("not.be.visible");
+		cy.get(".menu-container>.menu>li:first-child >a").click();
+		cy.get(".menu-container>.menu>li:first-child ul").should("not.be.visible");
 	});
 
 	it("click twice on data structures should open data structures sub menu", () => {
         cy.viewport(512, 768);
 		cy.get("nav .nav-toggle-container").should("be.visible");
 		cy.get("nav .nav-toggle-container").click();
-		cy.get("#data-structures").click();
-		cy.get("#data-structures").click();
-		cy.get("#sub-menu-1").should("be.visible");
+		cy.get(".menu-container>.menu>li:first-child >a").click();
+		cy.get(".menu-container>.menu>li:first-child >a").click();
+		cy.get(".menu-container>.menu>li:first-child ul").should("be.visible");
 	});
 
 	it("click on algorithms should close algorithms sub menu", () => {
         cy.viewport(512, 768);
 		cy.get("nav .nav-toggle-container").should("be.visible");
 		cy.get("nav .nav-toggle-container").click();
-		cy.get("#algorithms").click();
-		cy.get("#sub-menu-2").should("not.be.visible");
+		cy.get(".menu-container>.menu>li:nth-child(2) >a").click();
+		cy.get(".menu-container>.menu>li:nth-child(2) ul").should("not.be.visible");
 	});
 
 	it("click twice on algorithms should open algorithms sub menu", () => {
         cy.viewport(512, 768);
 		cy.get("nav .nav-toggle-container").should("be.visible");
 		cy.get("nav .nav-toggle-container").click();
-		cy.get("#algorithms").click();
-		cy.get("#algorithms").click();
-		cy.get("#algorithms").scrollIntoView();
-		cy.get("#sub-menu-2").should("be.visible");
+		cy.get(".menu-container>.menu>li:nth-child(2) >a").click();
+		cy.get(".menu-container>.menu>li:nth-child(2) >a").click();
+		cy.get(".menu-container>.menu>li:nth-child(2) >a").scrollIntoView();
+		cy.get(".menu-container>.menu>li:nth-child(2) ul").should("be.visible");
 	});
     
 	it("data structure click should show sub menu", () => {
 		cy.viewport("macbook-16");
-		cy.get("#data-structures").click();
-		cy.get("#sub-menu-1").should("be.visible");
+		cy.get(".menu-container>.menu>li:first-child >a").click();
+		cy.get(".menu-container>.menu>li:first-child ul").should("be.visible");
 	});
 
     it("data structure click twice should close sub menu", () => {
         cy.viewport("macbook-16");
-        cy.get("#data-structures").click();
-        cy.get("#data-structures").click();
-        cy.get("#sub-menu-1").should("not.be.visible");
+		cy.get(".menu-container>.menu>li:first-child >a").click();
+		cy.get(".menu-container>.menu>li:first-child >a").click();
+        cy.get(".menu-container>.menu>li:first-child ul").should("not.be.visible");
     });
 
 	it("algorithms click should show sub menu", () => {
 		cy.viewport("macbook-16");
-		cy.get("#algorithms").click();
-		cy.get("#sub-menu-2").should("be.visible");
+		cy.get(".menu-container>.menu>li:nth-child(2) >a").click();
+		cy.get(".menu-container>.menu>li:nth-child(2) ul").should("be.visible");
 	});
 
     // it.only("algorithms click twice should close sub menu", () => {
     //     cy.viewport("macbook-16");
-    //     cy.get("#algorithms").click();
-    //     cy.get("#algorithms").click();
-    //     cy.get("#sub-menu-2").should("not.be.visible");
+	// 	cy.get(".menu-container>.menu>li:nth-child(2) >a").click();
+	// 	cy.get(".menu-container>.menu>li:nth-child(2) >a").click();
+    //     cy.get(".menu-container>.menu>li:nth-child(2) ul").should("not.be.visible");
     // });
 });
