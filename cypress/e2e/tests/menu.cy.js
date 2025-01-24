55d000d4e3324f3d95b4e598696a2d5d28facf94
Pho3nyxX <60277772+Pho3nyxX@users.noreply.github.com>
2025-01-24 14:52:07 -0500

    algorithms sub menu tests pass

--- cypress/e2e/tests/menu.cy.js
+++ cypress/e2e/tests/menu.cy.js
@@ -17,13 +17,31 @@ describe("Menu", () => {
 		cy.get("#sub-menu-1").should("not.be.visible");
 	});
 
-	it.only("click twice on data structures should open data structures sub menu", () => {
+	it("click twice on data structures should open data structures sub menu", () => {
         cy.viewport(512, 768);
 		cy.get("nav .nav-toggle-container").should("be.visible");
 		cy.get("nav .nav-toggle-container").click();
 		cy.get("#data-structures").click();
 		cy.get("#data-structures").click();
 		cy.get("#sub-menu-1").should("be.visible");
+	});
+
+	it("click on algorithms should close algorithms sub menu", () => {
+        cy.viewport(512, 768);
+		cy.get("nav .nav-toggle-container").should("be.visible");
+		cy.get("nav .nav-toggle-container").click();
+		cy.get("#algorithms").click();
+		cy.get("#sub-menu-2").should("not.be.visible");
+	});
+
+	it.only("click twice on algorithms should open algorithms sub menu", () => {
+        cy.viewport(512, 768);
+		cy.get("nav .nav-toggle-container").should("be.visible");
+		cy.get("nav .nav-toggle-container").click();
+		cy.get("#algorithms").click();
+		cy.get("#algorithms").click();
+		cy.get("#algorithms").scrollIntoView();
+		cy.get("#sub-menu-2").should("be.visible");
 	});
     
     it("click twice should close", () => {
