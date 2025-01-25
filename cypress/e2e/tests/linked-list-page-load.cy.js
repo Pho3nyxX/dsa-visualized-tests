16e180f0d30fcfe753282669368264228c1c9b49
Pho3nyxX <60277772+Pho3nyxX@users.noreply.github.com>
2025-01-25 14:35:15 -0500

    linked list page load tests reorganized

--- cypress/e2e/tests/linked-list-page-load.cy.js
+++ cypress/e2e/tests/linked-list-page-load.cy.js
@@ -2,29 +2,21 @@ describe("Linked list page", () => {
     beforeEach(() => {
         cy.visit("");
         cy.viewport("macbook-16");
+        cy.get(".menu-title").should("be.visible");
+        cy.get(".menu-container>.menu>li:first-child >a").click();
+        cy.get(".menu-container>.menu>li:first-child ul").should("be.visible");
+        cy.get(".menu-container>.menu>li:first-child ul li:nth-child(2)").click();
     });
     
     it("should load after 'linked list' menu click", () => {
-        cy.get(".menu-title").should("be.visible");
-		cy.get(".menu-container>.menu>li:first-child >a").click();
-        cy.get(".menu-container>.menu>li:first-child ul").should("be.visible");
-        cy.get(".menu-container>.menu>li:first-child ul li:nth-child(2)").click();
         cy.url().should("include", "/linked-list");
     });
 
     it("title should display - linked list", () => {
-        cy.get(".menu-title").should("be.visible");
-		cy.get(".menu-container>.menu>li:first-child >a").click();
-        cy.get(".menu-container>.menu>li:first-child ul").should("be.visible");
-        cy.get(".menu-container>.menu>li:first-child ul li:nth-child(2)").click();
         cy.title().should('include', 'linked list');
     });
 
     it("content should have Linked List", () => {
-        cy.get(".menu-title").should("be.visible");
-		cy.get(".menu-container>.menu>li:first-child >a").click();
-        cy.get(".menu-container>.menu>li:first-child ul").should("be.visible");
-        cy.get(".menu-container>.menu>li:first-child ul li:nth-child(2)").click();
         cy.get(".content-display").contains("Linked List");
     });
 });
